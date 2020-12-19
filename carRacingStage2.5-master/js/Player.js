3f7390e025e9d82fb9a651794fa184015eb9dcca
devinmidha28 <69163361+devinmidha28@users.noreply.github.com>
2020-12-19 13:01:21 -0500

    Add files via upload

---
+++ carRacingStage2.5-master/js/Player.js
@@ -0,0 +1,46 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+  countread(){
+   var cr = database.ref ('count')
+    cr.on("value",function(data){
+      this.rank = data.val();
+    })
+  }
+  static write(abcd){
+    database.ref('/').update({
+      count:abcd})
+    }
+}        
